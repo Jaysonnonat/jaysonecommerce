6e0d5c0fc62257e34c6a7b68b295e104e64d071d
Jayson Nonat <jaysonnonat@gmail.com>
2021-01-12 17:33:09 -0700

    Added Image to the Item Page

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -13,25 +13,25 @@ export default {
             price: 12000
         },
         {
-            name: 'item_two',
+            name: 'item_three',
             id: 3,
             image: './images/bed.png',
             price: 30
         },
         {
-            name: 'item_two',
+            name: 'item_four',
             id: 4,
             image: './images/bed.png',
             price: 99
         },
         {
-            name: 'item_two',
+            name: 'item_five',
             id: 5,
             image: './images/bed.png',
             price: -10
         },
         {
-            name: 'item_two',
+            name: 'item_six',
             id: 6,
             image: './images/bed.png',
             price: 99.99
--- frontend/src/pages/ItemPage.js
+++ frontend/src/pages/ItemPage.js
@@ -2,10 +2,16 @@ import React from 'react'
 import data from './../data.js'
 
 export default function Item(props) {
+    console.log(props.match.params.id);
+    const item  = data.items.find(item => item.id == props.match.params.id);
     return (
         <div className="row center">
-            {props.match.params.id}
-            
+            <div>
+                <div className="header">
+                    {item.name}
+                </div>
+                <img className="large" src={"./."+item.image} alt="something"></img>
+            </div>
         </div>
     )
 }
